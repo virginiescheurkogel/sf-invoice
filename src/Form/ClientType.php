c717145c4802f596c3fe5128c9d5a7f647e71198
vivi <virginie.scheurkogel@gmail.com>
2021-03-26 14:02:48 +0100

    Gestion des clients te des adresses

---
+++ src/Form/ClientType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Client;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ClientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, ["label" => "Raison sociale"])
+            ->add('reference', TextType::class, ["label" => "Référence client"])
+            ->add('address', AddressType::class, ["label" => "Adresse"])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Client::class,
+        ]);
+    }
+}
